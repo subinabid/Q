ae89fde85b16c79d194606ee8c69fa3c8ba0d499
Subin Abid <subinabid@gmail.com>
2018-10-22 17:31:28 +0530

    Basic login

--- Q/settings.py
+++ Q/settings.py
@@ -69,7 +69,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Q.wsgi.application'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases

21bf0e9e5c9d38659677f934f8c04ed68ca07ee4
Subin Abid <subinabid@gmail.com>
2018-10-28 15:19:04 +0530

    tried to implement a form

--- quiz/views.py
+++ quiz/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import question
+from .forms import AnswerForm
 
 def quiz(request):
     return render(request, 'quiz/quiz.html',{})
 
 def que(request, que_id):
     que_list = question.objects.filter(question_id = que_id )
-    has_options = question.objects.only('has_options').filter(question_id = que_id)
-    has_media = question.objects.only('has_media').filter(question_id = que_id)
+    q= question.objects.get(pk = que_id)
+    has_options = q.has_options
+    has_media = q.has_media
     context = {'que_list': que_list, 'que_id' : que_id,'has_options':has_options, 'has_media':has_media}
     return render(request, 'quiz/que.html', context)

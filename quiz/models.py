8ffb7e0882e2e9be255a79d853685a4aa3234df4
Subin Abid <subinabid@gmail.com>
2019-01-09 23:22:06 +0530

    add a model for saving answers
    and migrate

--- quiz/models.py
+++ quiz/models.py
@@ -5,3 +5,8 @@ class question(models.Model):
     question_text = models.TextField()
     has_options = models.CharField(max_length=2)
     has_media = models.CharField(max_length=200)
+
+class answers(models.Model):
+    question_id = models.IntegerField()
+    user = models.CharField(max_length=10)
+    answer = models.CharField(max_length = 200)

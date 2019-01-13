d96478a7210338aa2d0b4fe5da136e029223c31c
Subin Abid <subinabid@gmail.com>
2019-01-14 00:57:33 +0530

    major overhaul
    Add time stamp to answers
    Add answerd in question table itself - for displaying later
    rewrite admin site to display timestamp

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import question, answers
 
-admin.site.register(question)
-admin.site.register(answers)
+class questionadmin(admin.ModelAdmin):
+    list_display = ('question_id', 'question_text', 'question_ans', 'has_options', 'has_media')
+
+class answersadmin(admin.ModelAdmin):
+    list_display = ('user','question_id' , 'answer','ans_date')
+
+admin.site.register(question, questionadmin)
+admin.site.register(answers, answersadmin)
--- quiz/models.py
+++ quiz/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class question(models.Model):
     question_id = models.IntegerField()
     question_text = models.TextField()
+    question_ans = models.TextField()
     has_options = models.CharField(max_length=2)
     has_media = models.CharField(max_length=200)
 
@@ -10,3 +11,4 @@ class answers(models.Model):
     question_id = models.IntegerField()
     user = models.CharField(max_length=10)
     answer = models.CharField(max_length = 200)
+    ans_date = models.DateTimeField(auto_now = True, blank = True)

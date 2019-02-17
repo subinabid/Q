47bb1411e9df4fa99e5a36970df5daa19e5dd14e
Subin Abid <subinabid@gmail.com>
2019-02-17 15:02:56 +0530

    Add quizid to identify the quiz
    trying to prevent multiple logins to the same quiz, using quiz id

--- quiz/models.py
+++ quiz/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
+class quizid(models.Model)
+    quiz_id = models.IntegerField()
+    quiz_date = models.DateTimeField()
+
 class question(models.Model):
+    quiz_id = models.ForeignKey(quiz_id, on_delete = models.CASCADE)
     question_id = models.IntegerField()
     question_text = models.TextField()
     question_ans = models.TextField()
@@ -8,11 +13,13 @@ class question(models.Model):
     has_media = models.CharField(max_length=200)
 
 class answers(models.Model):
+    quiz_id = models.IntegerField()
     question_id = models.IntegerField()
     user = models.CharField(max_length=10)
     answer = models.CharField(max_length = 200)
     ans_date = models.DateTimeField(auto_now = True, blank = True)
 
 class attempt(models.Model):
+    quiz_id = models.IntegerField()
     user = models.CharField(max_length = 10)
     login_date = models.DateTimeField(auto_now = True, blank = True)

6b89f40276443aea2c13679fe3693b782403c019
Subin Abid <subinabid@gmail.com>
2019-03-05 11:05:18 +0530

    misc

--- Q/urls.py
+++ Q/urls.py
@@ -4,6 +4,7 @@ from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
+    #path('', views.home, name='home'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
